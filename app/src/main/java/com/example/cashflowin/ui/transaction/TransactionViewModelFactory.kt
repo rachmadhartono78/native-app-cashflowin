07d4d981ea9b834c0cf987fe181046de4804ee3c
rachmadhartono78 <rahmadhartono78@gmail.com>
2026-03-05 03:43:11 +0700

    feat: Implement core UI for category management, asset transfers, and transaction handling.

---
+++ app/src/main/java/com/example/cashflowin/ui/transaction/TransactionViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.cashflowin.ui.transaction
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.cashflowin.api.TransactionRepository
+import com.example.cashflowin.ui.transaction.list.TransactionsViewModel
+
+class TransactionViewModelFactory(private val repository: TransactionRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AddTransactionViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AddTransactionViewModel(repository) as T
+        }
+        if (modelClass.isAssignableFrom(TransactionsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TransactionsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
